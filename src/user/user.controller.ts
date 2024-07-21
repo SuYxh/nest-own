e13844fdc377d5a336be2a571cd446facae5b501
yangxinhao <yangxinhao@zhuanzhuan.com>
2024-07-22 00:21:47 +0800

    feat: 增加 user模块

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,33 @@
-import { Controller, Get, InternalServerErrorException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
-
+import { User } from '../entity/user.entity';
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('error')
   findError() {
-    // throw new Error('chucuoll');
-    throw new InternalServerErrorException(`User with ID  not found`);
+    throw new Error('chucuoll');
+    // throw new InternalServerErrorException(`User with ID  not found`);
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<User[]> {
     return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<User> {
+    return this.userService.findOne(+id);
+  }
+
+  @Post()
+  create(@Body() user: User): Promise<User> {
+    return this.userService.create(user);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<void> {
+    return this.userService.remove(+id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../entity/user.entity';
 
 @Injectable()
 export class UserService {
-  findAll() {
-    return `This action returns all users`;
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+
+  findOne(id: number): Promise<User> {
+    return this.usersRepository.findOneBy({ id });
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.usersRepository.delete(id);
+  }
+
+  async create(user: User): Promise<User> {
+    return this.usersRepository.save(user);
   }
 }
