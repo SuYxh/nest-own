bea463fe0438c1767bc4182dfa0a4550acd2ff6e
yangxinhao <yangxinhao@zhuanzhuan.com>
2024-07-22 12:58:47 +0800

    feat: 配置跨域

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,12 @@ async function bootstrap() {
   app.useGlobalInterceptors(new CustomResponse());
   app.useGlobalFilters(new AllExceptionsFilter());
 
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(3001);
 }
 bootstrap();
